846e972abccee1fd6cc91564360e2a58002d1af0
abievour7 <159143078+abievour7@users.noreply.github.com>
2025-05-08 16:12:32 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // Variables
 const rootDomain = "stopaby.dpdns.org"; // Ganti dengan domain utama kalian
-const serviceName = "FWB"; // Ganti dengan nama workers kalian
+const serviceName = "jangan"; // Ganti dengan nama workers kalian
 const apiKey = "SDqihF3hFjNurBQ0lDJTM6EAVRKVgGmAB6CbAy-8"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = "abydodik5@gmail.com"; // Ganti dengan email yang kalian gunakan
 const accountID = "8f108e466e2710c1381beef272323d64"; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
